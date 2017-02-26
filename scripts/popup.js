76e4bd5c8c14b8dbb114c0617f9f9d6669f9f8ff
Robert J. Kusznier <rj-kusznier@inventati.org>
2017-02-26 12:57:18 +0100

    Implemented displaying randomly chosen number of people

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,8 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const POPUP_CSS_CLASS = "popup";
   const CLOSE_CSS_CLASS = "popup_close";
   const POPUP_CLOSED_CSS_CLASS = "popup_is-closed";
+  const PEOPLE_NUMBER_CSS_CLASS = "popup_people-number";
+  const PEOPLE_NUMBER_MIN = 6;
+  const PEOPLE_NUMBER_MAX = 20;
 
   let popups = document.getElementsByClassName(POPUP_CSS_CLASS);
+  let peopleNumbers = document.getElementsByClassName(PEOPLE_NUMBER_CSS_CLASS);
 
 
   function setupPopup(popup) {
@@ -20,6 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
+
+  /* Populates people number container with random integer from defined
+     range. */
+  function setupPeopleNumber(peopleNumber) {
+    let randomNumber = Math.random() * (PEOPLE_NUMBER_MAX - PEOPLE_NUMBER_MIN + 1);
+    randomNumber = Math.floor(randomNumber) + PEOPLE_NUMBER_MIN;
+    peopleNumber.innerHTML = randomNumber.toString();
+  }
+
+
   Array.prototype.forEach.call(popups, setupPopup);
+  Array.prototype.forEach.call(peopleNumbers, setupPeopleNumber);
 
 });
